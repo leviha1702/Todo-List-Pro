a87b734a30c80786ad6bd043a95bcd05526f67ec
diem <levietha170205@gmail.com>
2025-06-19 18:19:33 +0700

    code learn FE

--- src/components/ContentHome.jsx
+++ src/components/ContentHome.jsx
@@ -1,9 +1,34 @@
 import "../styles/page1.css";
+import React from "react";
+import axios from "axios";
 
 const ContentHome = () => {
+  const [state, setState] = React.useState(false);
+  const [password, setPassword] = React.useState(false);
+
+  React.useEffect(() => {
+    axios
+      .get("https://api.example.com/home")
+      .then((response) => {
+        console.log("Todos:", response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the todos!", error);
+      });
+  }, []);
+
+  const handleClick = () => {
+    setState(!state);
+  };
+
+  const handlePassword = () => {
+    setPassword(!password);
+  };
   return (
     <div className="content">
       <h1>Content Home</h1>
+      <button onClick={handleClick}>Click</button>
+      <input type="password" onChange={handlePassword} />
     </div>
   );
 };
